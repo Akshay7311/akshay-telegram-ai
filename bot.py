a9b605f0db59da19ef1ef342ac311e57d94c1f18
AKshay7311 <akshay7311a@gmail.com>
2026-02-26 23:34:44 +0530

    stable render version

--- bot.py
+++ bot.py
@@ -38,7 +38,14 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
-    app.run_polling()
+
+    port = int(os.environ.get("PORT", 10000))
+
+    app.run_webhook(
+        listen="0.0.0.0",
+        port=port,
+        webhook_url=f"https://{os.environ.get('RENDER_EXTERNAL_HOSTNAME')}/{BOT_TOKEN}"
+    )
 
 
 if __name__ == "__main__":
